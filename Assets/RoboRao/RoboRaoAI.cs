ad80a97ad0d82150cf4defb546eac6f07dd69c20
Chaitanya21-bit <cyrao378@gmail.com>
2022-07-24 14:12:54 +0530

    smooth movements of roborao

--- Assets/RoboRao/RoboRaoAI.cs
+++ Assets/RoboRao/RoboRaoAI.cs
@@ -7,14 +7,38 @@ public class RoboRaoAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent navMeshAgent;
+    Rigidbody rb;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+       
         navMeshAgent.SetDestination(target.position);
+        float distanceFromPlayer = Vector3.Distance(navMeshAgent.transform.position, target.position);
+        if (distanceFromPlayer < navMeshAgent.stoppingDistance)
+        {
+            navMeshAgent.isStopped = true;
+            RoboraoIsStationary();
+        }
+        else
+        {
+            navMeshAgent.isStopped = false;
+        }
     }
+
+    bool RoboraoIsStationary()
+    {
+        if (this.rb.velocity == Vector3.zero)
+        {
+            Debug.Log("reached the player");
+            return true;
+        }
+        return false;
+    }
+
 }
