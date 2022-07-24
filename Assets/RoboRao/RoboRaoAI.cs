b7583f949f31da2047ecb9abc644230987c92756
Saurrabhh <saurabhkyadav99@gmail.com>
2022-07-24 22:16:50 +0530

    robo rao fix

--- Assets/RoboRao/RoboRaoAI.cs
+++ Assets/RoboRao/RoboRaoAI.cs
@@ -7,31 +7,38 @@ public class RoboRaoAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent navMeshAgent;
-    [SerializeField] Vector3 offset = Vector3.zero;
+    [SerializeField] int offset = 1;
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         
+
     }
 
    
     void Update()
     {
-       
-        navMeshAgent.SetDestination(target.position);
+        float z_offset = Mathf.Cos(Mathf.Deg2Rad * (target.eulerAngles.y - 45)) * offset;
+        float x_offset = Mathf.Sin(Mathf.Deg2Rad * (target.eulerAngles.y - 45)) * offset;
+        Debug.Log(x_offset + " " + z_offset);
+        Vector3 off = new Vector3(x_offset, 0, z_offset);
+
+
+        navMeshAgent.SetDestination(target.position + off);
         float distanceFromPlayer = Vector3.Distance(navMeshAgent.transform.position, target.position);
-        if (distanceFromPlayer <= navMeshAgent.stoppingDistance)
+        if (distanceFromPlayer < navMeshAgent.stoppingDistance)
         {
             navMeshAgent.isStopped = true;
-            //Move();
-            //navMeshAgent.Move(offset);
-            //navMeshAgent.transform.position = -target.position;
+            //navMeshAgent.Move(off);
             Debug.Log("reached the player");
-            
+
         }
         else
         {
             navMeshAgent.isStopped = false;
+            
+            Debug.Log("Not reached the player");
+
         }
     }
 
