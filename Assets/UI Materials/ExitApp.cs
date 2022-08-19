075960fc784fa60349ecadae5bd4711155cd07a8
Saurrabhh <saurabhkyadav99@gmail.com>
2022-08-19 12:59:33 +0530

    added exit btn

---
+++ Assets/UI Materials/ExitApp.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitApp : MonoBehaviour
+{
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    
+}
