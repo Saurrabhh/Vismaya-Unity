20d3ab03327c0eacab778c8f28200e0aa0d09c8a
Chaitanya21-bit <cyrao378@gmail.com>
2022-08-22 14:34:18 +0530

    added artifact tp

---
+++ Assets/ArtifactManager/Pillar.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pillar : MonoBehaviour
+{
+    public Vector3 pillarPosition;
+    public bool hasArtifact = false;
+    //public static List<Vector3> pillarPosList = new List<Vector3>();
+    //[SerializeField]public GameObject pillar;
+
+    private void Start()
+    {
+        AppendPosition();
+        
+    }
+    public void AppendPosition()
+    {
+
+        Player.pillarInfo.Add(this);
+        
+       // pillarPosition = pillar.transform.position;
+        
+       // pillarPosList.Add(pillarPosition);
+       // for(int i = 0; i < pillarPosList.Count; i++)
+        //{
+        //    Debug.Log(pillarPosList[0]);
+        //}
+    }
+
+   
+
+    private void Update()
+    {
+        
+    }
+}
--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -14,9 +14,10 @@ public class Player : MonoBehaviour
     public static List<Tool> totalTools = new List<Tool>();
     public static List<Tool> activeTools = new List<Tool>();
     public static List<Artifact> artifactsList = new List<Artifact> ();
+    public static List<Pillar> pillarInfo = new List<Pillar> ();
     public static bool hasDugged = false;
 
-    
+   
     public void SavePlayer()
     {
         //add scene index
@@ -49,15 +50,22 @@ public class Player : MonoBehaviour
         
         if (hasDugged)
         {
-            ArtifactInstantiate(artifactsList[0].m_gameObject);
+            ArtifactInstantiate(artifactsList[0].m_gameObject, Vector3.positiveInfinity, pillarInfo[0]);
             hasDugged = false;
         }
          
     }
 
-    public void ArtifactInstantiate(GameObject prefab)
+    public void ArtifactInstantiate(GameObject prefab, Vector3 pos, Pillar pillar)
     {
-        Instantiate(prefab, new Vector3(4, 1, 5), Quaternion.identity).gameObject.SetActive(true);
+        Vector3 finalPosition;
+        finalPosition.x = pillar.transform.position.x;
+        finalPosition.y = pillar.transform.position.y + 1;
+        finalPosition.z = pillar.transform.position.z;
+        //Instantiate(prefab, pos, Quaternion.identity).gameObject.SetActive(true);
+        prefab.transform.SetParent(pillar.transform);
+        prefab.transform.position = finalPosition;
+        prefab.gameObject.SetActive(true);
     }
 
 
