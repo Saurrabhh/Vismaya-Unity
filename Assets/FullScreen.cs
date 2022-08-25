4eeb9db41aaba186bfae6a9ebcc9a2a5045fcad9
Saurrabhh <saurabhkyadav99@gmail.com>
2022-08-25 10:05:43 +0530

    updated museum

---
+++ Assets/FullScreen.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.fullScreen = true;
+    }
+
+    
+}
