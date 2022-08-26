d4266e81de920f94e2f05ecc5dc9f89753ab5116
Saurrabhh <saurabhkyadav99@gmail.com>
2022-08-26 17:28:13 +0530

    dfrbbre

--- Assets/demo3.cs
+++ Assets/demo3.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class demo3 : MonoBehaviour
 {
     public GameObject trenching;
+    public GameObject trenching2;
+    public GameObject tools;
     public GameObject techniques;
     public void Trench()
     {
         trenching.SetActive(true);
         techniques.SetActive(false);
+        trenching2.SetActive(false);
+        tools.SetActive(false);
     }
 
     public void Techniques()
     {
         techniques.SetActive(true);
         trenching.SetActive(false);
+        trenching2.SetActive(false);
+        tools.SetActive(false);
     }
+
+    public void Trench2()
+    {
+        techniques.SetActive(false);
+        trenching.SetActive(false);
+        trenching2.SetActive(true);
+        tools.SetActive(false);
+    }
+
+    public void Toool()
+    {
+        techniques.SetActive(false);
+        trenching.SetActive(false);
+        trenching2.SetActive(false);
+        tools.SetActive(true);
+    }
+
 }
